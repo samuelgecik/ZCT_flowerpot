eece3be720b184e5e2a597bebfeb66fa16086e5f
samo <samuel.gecik@student.tuke.sk>
2021-04-28 21:03:05 +0200

    blazorDbData

---
+++ DBviewer/Data/PotState.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace DBviewer.Data
+{
+    public class PotState
+    {
+        public DateTime Date { get; set; }
+
+        public int Moisture { get; set; }
+
+        public string Luminance { get; set; }
+    }
+}
